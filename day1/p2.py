4edb7946e0bc3ba83874a73c702f80e11bcd4941
nluu175 <lnminh175@gmail.com>
2025-12-01 21:44:18 -0700

    feat: day 1 - add more comments + readme

--- day1/p2.py
+++ day1/p2.py
@@ -14,9 +14,14 @@ def execute_rotation(starting_point, moves):
     cycles_traveled = 0
 
     if moves < 0:
+        # aka. Check if we wrapped around (crossed 0 at least once)
+        # new_point > starting_point means we cross 0
+        # starting_point != 0 is used for eliminating false positive
         if new_point > starting_point and starting_point != 0:
             cycles_traveled += 1
     else:
+        # new_point < starting_point means we cross 0
+        # new_point != 0 since it's already handled in main loop
         if new_point < starting_point and new_point != 0:
             cycles_traveled += 1
 
@@ -26,20 +31,20 @@ def execute_rotation(starting_point, moves):
 
 
 def main():
-    starting_point = 50
+    current_point = 50
     zero_counter = 0
 
     with open("input.txt", "r") as file:
         for line in file:
             moves = decode_rotation(line.strip())
-            prev_point = starting_point
-            starting_point, cycles_traveled = execute_rotation(starting_point, moves)
+            prev_point = current_point
+            current_point, cycles_traveled = execute_rotation(current_point, moves)
 
             print(
-                f"Prev: {prev_point},     Move: {moves},      Current: {starting_point},      Cycles Traveled: {cycles_traveled}"
+                f"Prev: {prev_point},     Move: {moves},      Current: {current_point},      Cycles Traveled: {cycles_traveled}"
             )
 
-            if starting_point == 0:
+            if current_point == 0:
                 zero_counter += 1
             zero_counter += cycles_traveled
 
